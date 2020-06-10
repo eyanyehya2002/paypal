4e3745283ed623e4fc2b10a2c417e2bdfb88551d
Eyan Yehya <eyanyehya@gmail.com>
2020-06-10 16:55:26 +0400

    second

---
+++ django_project/ecommerce_app/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('product/<int:product_id>/<slug:product_slug>/',views.show_product, name='product_detail'),
+    path('cart/', views.show_cart, name='show_cart'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('process-payment/', views.process_payment, name='process_payment'),
+    path('payment-done/', views.payment_done, name='payment_done'),
+    path('payment-cancelled/', views.payment_canceled, name='payment_cancelled'),
+    path('subscribe/', views.subscription, name='subscription'),
+    path('process_subscription/', views.process_subscription, name='process_subscription'),
+]
